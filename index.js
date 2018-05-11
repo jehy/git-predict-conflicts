dde9c125c5aa1baa6369fb79fc8c27c8a60426cf
Jehy <fate@jehy.ru>
2018-05-11 20:17:14 +0300

    almost working

--- index.js
+++ index.js
@@ -3,29 +3,38 @@ const fs = require('fs-extra');
 const path = require('path');
 const {spawn} = require('child_process');
 const Promise = require('bluebird');
-const debug = require('debug')('predict');
+const debug = require('debug');
+const {lstatSync, readdirSync} = require('fs');
+const {join} = require('path');
+
+const log = {
+  error: debug('predict:err'),
+  info: debug('predict:info'),
+};
 
 function spawnPromise(program, args, options) {
 
+  const display = `Running ${program} ${args.join(' ')}`;
+  log.info(display);
   return new Promise((resolve, reject) => {
 
-    let data = '';
-    let err = '';
+    let data = [];
+    let err = [];
     const ps = spawn(program, args, options);
     ps.stdout.on('data', (newData) => {
-      data += newData;
+      data.push(newData);
     });
 
     ps.stderr.on('data', (newData) => {
-      err += newData;
+      err.push(newData);
     });
 
     ps.on('close', (code) => {
       if (code !== 0) {
-        reject(err);
+        reject(new Error(`${display}: ${err.join('').substr(-200)} ${data.join('').substr(-200)}`));
         return;
       }
-      resolve(data);
+      resolve(data.join(''));
     });
   })
 }
@@ -33,6 +42,8 @@ function spawnPromise(program, args, options) {
 program
   .version('0.1.0')
   .option('-p, --path <path>', 'Git path for your repo')
+  .option('-d, --diff', 'Generate diff files')
+  .option('-c, --conflicts', 'Generate conflicts file')
   .parse(process.argv);
 
 // console.log(JSON.stringify(program, null, 3));
@@ -63,17 +74,38 @@ function fetchDiffs() {
         .split('\n')
         .map((line) => line.split('refs/heads/')[1])
         .filter((branchName) => branchName);
-      debug(`Remote data: ${JSON.stringify(branches, null, 3)}`);
-      debug(`Fetching ${branches.length} remote branches...`);
+      log.info(`Remote data: ${JSON.stringify(branches, null, 3)}`);
+      log.info(`Fetching ${branches.length} remote branches...`);
       return Promise.map(branches, (branch) => {
         return spawnPromise('git', ['fetch', 'origin', branch], {cwd: TMPDIR});
       })
         .then(() => branches);
     })
     .then((branches) => {
-      debug(`Fetched all branches, checking diff`);
+      log.info(`Pulling master to remote branches...`);
+      const branchesMasterConflict = [];
+
+      return spawnPromise('git', ['reset', '--hard', 'HEAD'], {cwd: 'tmp'})
+        .then(() => Promise.map(branches, (branch) => {
+          return spawnPromise('git', ['checkout', branch], {cwd: 'tmp'})
+            .then(() => spawnPromise('git', ['pull', '.', 'master'], {cwd: 'tmp'}))
+            .catch((err) => {
+              log.error(`Error: ${err}`);
+              branchesMasterConflict.push(branch);
+              return spawnPromise('git', ['checkout', '-f', branch], {cwd: 'tmp'});
+            })
+        }, {concurrency: 1})
+          .then(() => [branches, branchesMasterConflict]))
+    })
+    .then(([branches, branchesMasterConflict]) => {
+      log.info(`${branchesMasterConflict.length}/${branches.length} branches in conflict with master:`);
+      log.info(`${branchesMasterConflict.join('\n')}`);
+      return branches.filter((branch) => !branchesMasterConflict.includes(branch));
+    })
+    .then((branches) => {
+      log.info(`Fetched all branches, checking diff`);
       return Promise.map(branches, (branch) => {
-        const getData = spawnPromise('git', ['diff', '--name-status', `origin/${branch}..origin/master`], {cwd: TMPDIR});
+        const getData = spawnPromise('git', ['diff', '--name-status', `${branch}..master`], {cwd: 'tmp'});
         const getAuthor = spawnPromise('git', ['log', `origin/${branch}`, '-1', '--pretty=format:%an'], {cwd: TMPDIR});
         return Promise.all([getData, getAuthor])
           .then(([res, author]) => {
@@ -85,16 +117,63 @@ function fetchDiffs() {
                 return {action: data[0], file: data[1]};
               });
             const fileData = {changes: data, author};
-            const dir = `tmp/diff/${branch}`;
-            return fs.ensureDir(dir)
-              .then(() => fs.writeFile(path.join(dir, 'diff.json'), JSON.stringify(fileData, null, 3)));
+            const file = `tmp/diff/${branch}.json`;
+            return fs.ensureDir('tmp/diff')
+              .then(() => fs.writeFile(file, JSON.stringify(fileData, null, 3)));
           })
       })
     })
     .then(() => {
-      debug('Got all diffs!');
+      log.error('Got all diffs!');
+    })
+    .catch((err) => {
+      throw err;
     });
 }
 
-fetchDiffs();
-//git ls-remote --heads origin
+function getFiles(source) {
+  return readdirSync(source).map(name => join(source, name)).filter((file) => lstatSync(file).isFile());
+}
+
+if (program.diff) {
+  fetchDiffs();
+}
+else if (program.conflicts) {
+  const fileNames = getFiles('tmp/diff');
+  const conflicts = [];
+  Promise.map(fileNames, (fileName) => fs.readJson(fileName).then((data)=>[fileName, data]))
+    .then((data) => {
+      data.forEach((element, index) => {
+        const [filename, contents] = element;
+        const task = filename.replace('.json', '').replace('/tmp/diff', '');
+        data.forEach((elementCompare, indexCompare) => {
+          if (index === indexCompare) {
+            return;
+          }
+          const [filenameCompare, contentsCompare] = elementCompare;
+          const taskCompare = filenameCompare.replace('.json', '').replace('/tmp/diff', '');
+          if (conflicts.some((conflict) => conflict.task1 === taskCompare && conflict.task2 === task)) {
+            return; //avoid duplication
+          }
+          if (contents.changes===undefined) {
+            log.error(`WTF? ${JSON.stringify(contents)}`);
+            return;
+          }
+          if (contentsCompare.changes===undefined) {
+            log.error(`WTFCOMPARE? ${JSON.stringify(contentsCompare)}`);
+            return;
+          }
+          const onlyFiles = Object.values(contents.changes).map((obj) => obj.file);
+          const onlyFilesCompare = Object.values(contentsCompare.changes).map((obj) => obj.file);
+          const intersections = onlyFiles.filter((changed) => onlyFilesCompare.includes(changed));
+          if (intersections.length) {
+            conflicts.push({task1: task, task2: taskCompare, files: intersections});
+          }
+        });
+      });
+      return fs.writeFile('tmp/diff/conflicts.json', JSON.stringify(conflicts, null, 3));
+    })
+}
+else {
+  throw new Error('No options provided!');
+}
